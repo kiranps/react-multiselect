4bdad7cedac7c427fc14c5bac47146a68f856a28
Kiran PS <pskirann@gmail.com>
2018-05-31 18:52:22 +0530

    arrow icon alignment fixed

--- src/Arrow.js
+++ src/Arrow.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Arrow = ({height, width, color, style, isOpen}) =>
   isOpen ?
-    <svg width={width} height={height} style={{boxSizing: "border-box", marginTop: "2px"}} fill={color} viewBox="0 0 24 24">
+    <svg width={width} height={height} style={{boxSizing: "border-box"}} fill={color} viewBox="0 0 20 14">
       <path d="M7,15L12,10L17,15H7Z" />
     </svg>
     :
-    <svg width={width} height={height} style={{boxSizing: "border-box", marginTop: "2px"}} fill={color} viewBox="0 0 24 24">
+    <svg width={width} height={height} style={{boxSizing: "border-box"}} fill={color} viewBox="0 0 20 14">
       <path d="M7,10L12,15L17,10H7Z" />
     </svg>
 
